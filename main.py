f418b84346fef55bd1a0e9296de70024789d86c0
MichelPrincy <157823302+MichelPrincy@users.noreply.github.com>
2026-02-04 23:41:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ class TikTokTaskBot:
     # ---------- MISE À JOUR ----------
     def update_script(self):
         print(f"{CYAN}🌐 Vérification mise à jour...{RESET}", flush=True)
-        url = "https://raw.githubusercontent.com/MichelPrincy/telebot/main/main.py"
+        url = "https://raw.githubusercontent.com/MichelPrincy/jaden/main/main.py"
         try:
             response = requests.get(url)
             if response.status_code == 200:
