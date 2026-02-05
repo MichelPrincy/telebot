0068e8a819ad53153c3c2e51a326a0e535b44cd2
MichelPrincy <157823302+MichelPrincy@users.noreply.github.com>
2026-02-05 12:03:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -307,11 +307,11 @@ votre limite de CashCoin.
                     sent = False
                     send_btn = self.d(resourceIdMatches="(?i).*id/(send_btn|publish_button|comment_publish_img)")
                     if send_btn.exists:
-                        self.d.click(978, 1153)
+                        self.d.click(978, 1250)
                         sent = True
                     
                     if not sent:
-                        self.d.click(978, 1153) 
+                        self.d.click(978, 1250) 
                         sent = True
                     
                     print(f"{GREEN}    -> Commentaire envoyé !{RESET}")
